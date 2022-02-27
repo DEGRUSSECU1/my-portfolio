28847e9dd16203e14ef478aa82e280249698aafd
Diego Russo <diego25.russo@gmail.com>
2022-02-27 16:04:10 +0000

    my page

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('#portfolio .thumbnail').hover(function(){
+      $(this).children(".cust-caption").slideDown();
+    },
+    function(){
+      $(this).children(".cust-caption").fadeOut();
+    });
+  
+      
+    // spy and scroll menu boogey
+    $("#navbar a.hash").click( function(e) {
+    
+      // prevent default anchor click behavior
+      e.preventDefault();
+  
+      // store hash
+      var hash = this.hash;
+  
+      // Destination
+      var scrollOffset = $(this.hash).offset().top;
+  
+      // animate
+      $('html, body').animate({
+        scrollTop: scrollOffset
+      }, 500, function(){
+        window.location.hash = hash;
+      }) 
+    })
+    
+  });
+
+
+  //img size 1600 pixels wide x 400 pixels tall
+
+
